a3484c030ff2bd9f1e5f89aa21eaca8eb061fad2
Marcin <marcin.konow@lczyk.xyz>
2021-12-11 19:58:14 +0000

    faster day08_2

--- day08_2.jl
+++ day08_2.jl
@@ -1,83 +1,92 @@
 using DelimitedFiles
 
-data_file = "./data/test/day08_input.txt"
-# data_file = "./data/full/day08_input.txt"
+# data_file = "./data/test/day08_input.txt"
+data_file = "./data/full/day08_input.txt"
 data = readdlm(data_file, ' ', String)
 
 inputs = data[:, 1:10]
 outputs = data[:, 12:end]
 
-# copied and slightly adjusted, from https://github.com/JuliaMath/Combinatorics.jl
-# TODO: this can be done some much more efficiently!
-#       this is O(k*n^2) (is it??) and can be easilly O(n). 
-#       write and PR to Combinatorics.jl
-# TODO: fix certain numbers by the digit 4 and 7 (simplest), can reduce 
-function kthperm(n::Integer, k::Integer)
-    a = collect(1:n)
-    n == 0 && return a
-    f = factorial(oftype(k, n))
-    0 < k <= f || throw(ArgumentError("permutation k must satisfy 0 < k ≤ $f, got $k"))
-    k -= 1 # make k 1-indexed
-    for i = 1:n-1
-        f ÷= n - i + 1
-        j = k ÷ f
-        k -= j * f
-        j += i
-        elt = a[j]
-        for d = j:-1:i+1
-            a[d] = a[d-1]
-        end
-        a[i] = elt
-    end
-    return a
-end
+const Wiring = Set{Char}
 
-target = [
-    "abcefg",
-    "cf",
-    "acdeg",
-    "acdfg",
-    "bcdf",
-    "abdfg",
-    "abdefg",
-    "acf",
-    "abcdefg",
-    "abcdfg",
-]
-sorted_target = sort(target)
-wire_labels = collect("abcdefg")
+# overload minus with some helpful set operations
+import Base.-
+@inline -(s1::Wiring, s2::Wiring) = setdiff(s1, s2)
+@inline -(s1::Wiring, s2::Char) = setdiff(s1, Wiring(s2))
+@inline -(s1::Vector{Wiring}, s2::Wiring) = [e - s2 for e in s1]
+@inline -(s1::Vector{Wiring}, s2::Char) = [e - Wiring(s2) for e in s1]
 
-translate(perm, digit) =
-    join(sort([wire_labels[findfirst(wire_labels[perm] .== d)] for d in digit]))
+# do this instead of boradcasting
+# bordcast like Vector{Wiring} .- Wiring gets confused, since both left and right have length
+
+wiring_map = Dict{String,Int}(
+    "abcefg" => 0,
+    "cf" => 1,
+    "acdeg" => 2,
+    "acdfg" => 3,
+    "bcdf" => 4,
+    "abdfg" => 5,
+    "abdefg" => 6,
+    "acf" => 7,
+    "abcdefg" => 8,
+    "abcdfg" => 9,
+)
+
+import Base.map, Base.reduce, Base.filter
+map(f) = Base.Fix1(map, f)
+map(d::Dict) = Base.Fix1(map, x -> d[x])
+reduce(f) = Base.Fix1(reduce, f)
+filter(f) = Base.Fix1(filter, f)
+
+# Sort string
+stringsort = join ∘ sort ∘ collect
+
+# digit base 10 array to number
+digitise =
+    (x -> x[2]) ∘ reduce((x, y) -> (0, x[2] + 10^(y[1] - 1) * y[2])) ∘ enumerate ∘ reverse
+
+# Helper functions for searching through the input vector
+filtern(n) = filter(x -> x[1] == n) ∘ map(x -> (length(x), x))
+single = Set ∘ x -> x[1][2]
+filterone = single ∘ filtern(1)
+many = map(Set ∘ x -> x[2])
+item(x::Wiring) = collect(x)[1] # get first item from a set
 
 N = size(inputs, 1)
 translated_outputs = zeros(Int, N)
 for (row_index, (input, output)) in enumerate(zip(eachrow(inputs), eachrow(outputs)))
-    perm = undef
-    for k = 1:factorial(7)
-        perm = kthperm(7, k)
-        translated_input = sort([translate(perm, digit) for digit in input])
-        translated_input == sorted_target && break
-    end
-    if perm === undef
-        throw(ArgumentError("no permutation on row $row_index found"))
-    else
-        println("$row_index / $N | permutation found: $perm")
-    end
 
-    # @show perm
-    # @show output[1]
-    # @show findall(target .== translate(perm, output[1]))
-    translated_output = sum(
-        10 .^ (3:-1:0) .*
-        [findfirst(target .== translate(perm, digit)) - 1 for digit in output],
+    # Find 
+    two = input |> filtern(2) |> single
+    three = input |> filtern(3) |> single
+    four = input |> filtern(4) |> single
+    five = input |> filtern(5) |> many
+
+    a = three - two |> item
+    diff = five - four - a
+    g = diff |> filterone |> item
+    e = (diff - g) |> filterone |> item
+    d = (five - three - g) |> filterone |> item
+    b = (five - e - g - d - three) |> filterone |> item
+    c = (five - e - g - d - a) |> filterone |> item
+    f = two - c |> item
+
+    decode_map = Dict{Char,Char}(
+        a => 'a',
+        b => 'b',
+        c => 'c',
+        d => 'd',
+        e => 'e',
+        f => 'f',
+        g => 'g',
     )
-    translated_outputs[row_index] = translated_output
+
+    translated_outputs[row_index] =
+        output |> map(stringsort ∘ map(decode_map)) |> map(wiring_map) |> digitise
 end
 
 sum_of_outputs = sum(translated_outputs)
 
-@show translated_outputs
 @show sum_of_outputs
 
 answer = sum_of_outputs

05014e18b232decd97cb015eba7046b9c7cf6bab
Marcin <marcin.konow@lczyk.xyz>
2021-12-17 02:22:31 +0000

    removed prirority queue and just used a queue

--- day15_1.jl
+++ day15_1.jl
@@ -17,28 +17,20 @@ visited_map = falses((N, M))
 
 neighbours = (CartesianIndex(-1, 0), CartesianIndex(1, 0), CartesianIndex(0, -1), CartesianIndex(0, 1))
 
-# TODO: try implementing one's own priority queue
-#       maybe adapt minheap...?
-
-using DataStructures
-
 begin
-    local visit_queue = PriorityQueue{CartesianIndex{2}, Int}(CartesianIndex(1,1)=>0)
+    local visit_queue = Vector{CartesianIndex{2}}([CartesianIndex(1,1)])
     local epoch = 0
     while !isempty(visit_queue)
-        current_node = dequeue!(visit_queue)
+        current_node = pop!(visit_queue)
+        !visited_map[current_node] || continue
         visited_map[current_node] = true
-
-        # cost_of_this_node = cost_map[current_node] + chitons[current_node]
 
         for neighbour in (current_node + x for x in neighbours)
             (checkbounds(Bool, cost_map, neighbour) && !visited_map[neighbour]) || continue
-            cost_of_this_node = cost_map[current_node] + chitons[neighbour]
-
-            if cost_of_this_node < cost_map[neighbour]
-                cost_map[neighbour] = cost_of_this_node
-                # @show visit_queue, neighbour
-                enqueue!(visit_queue, neighbour, cost_of_this_node)
+            cost_of_neighbour = cost_map[current_node] + chitons[neighbour]
+            if cost_of_neighbour < cost_map[neighbour]
+                cost_map[neighbour] = cost_of_neighbour
+                pushfirst!(visit_queue, neighbour)
             end
         end
 
@@ -47,14 +39,14 @@ begin
             done = round(sum(visited_map)/(N*M)*100; digits=1)
             println("$done % done")
 
-            for row in eachrow(visited_map)
-                for node in row
-                    symbol = node ? 'X' : '.'
-                    color = node ? :blue : :light_black
-                    printstyled(symbol; color = color);
-                end
-                print("\n")
-            end
+            # for row in eachrow(visited_map)
+            #     for node in row
+            #         symbol = node ? 'X' : '.'
+            #         color = node ? :blue : :light_black
+            #         printstyled(symbol; color = color);
+            #     end
+            #     print("\n")
+            # end
         end
     end
 end

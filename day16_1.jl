4f35e8b998d671771f3107c52fbd71e09dee9012
Marcin <marcin.konow@lczyk.xyz>
2021-12-17 17:06:12 +0000

    day 16 done

---
+++ day16_1.jl
@@ -0,0 +1,132 @@
+using DelimitedFiles
+
+data_file = "./data/test/day16_input.txt"
+# data_file = "./data/full/day16_input.txt"
+data = readdlm(data_file, '\n', String)[:]
+
+# Patch map, reduce and filter with Fix1 for piping and composition
+import Base.map, Base.reduce, Base.filter, Base.parse
+map(f) = Base.Fix1(map, f)
+parse(t) = Base.Fix1(parse, t)
+# reduce(f) = Base.Fix1(reduce, f)
+# filter(f) = Base.Fix1(filter, f)
+
+hex2bits = BitVector ∘ map(parse(Bool)) ∘ collect ∘ join ∘ map(bitstring) ∘ hex2bytes
+bits2int = Base.Fix1(sum, ((i, x),) -> x << (i - 1)) ∘ enumerate ∘ reverse
+
+"""
+Read n bits from the bit stream, and push the pointer along the way.
+The pointer is the only hign which gets modified.
+"""
+function read_n_bits(bits::BitVector, pointer::Int, n::Int)
+    len = length(bits)
+    @assert (pointer+n-1) <= len "$pointer + $n larger than the length of the bits array! ($len)"
+    bits = bits[pointer:(pointer+n-1)]
+    pointer += n
+    return bits, pointer
+end
+
+function parse_packet(bits::BitVector, pointer::Int)
+    version, new_pointer = read_n_bits(bits, pointer, 3)
+    version = bits2int(version)
+    typeid, _ = read_n_bits(bits, new_pointer, 3)
+    typeid = bits2int(typeid)
+
+    if typeid == 4
+        println("literal packet | version: $version | typeid: $typeid")
+        return parse_literal_packet(bits, pointer)
+    else
+        println("operator packet | version: $version | typeid: $typeid")
+        return parse_operator_packet(bits, pointer)
+    end
+end
+
+"""
+Parse a literal packet out of the bit stream
+"""
+function parse_literal_packet(bits::BitVector, pointer::Int)
+    version, pointer = read_n_bits(bits, pointer, 3)
+    version = bits2int(version)
+    typeid, pointer = read_n_bits(bits, pointer, 3)
+    typeid = bits2int(typeid)
+    @assert typeid == 4 "typeid ($typeid) != 4. Not a literal packet."
+
+    # Read groups until a terminal one is found
+    value = BitVector([])
+    indicator_bit = true
+    while indicator_bit
+        group, pointer = read_n_bits(bits, pointer, 5)
+        indicator_bit = group[1]
+        value = vcat(value, group[2:end])
+    end
+    value = bits2int(value)
+
+    return (version, typeid, value), pointer
+end
+
+"""
+Parse an operator packet out of the bit stream
+"""
+function parse_operator_packet(bits::BitVector, pointer::Int)
+    version, pointer = read_n_bits(bits, pointer, 3)
+    version = bits2int(version)
+    typeid, pointer = read_n_bits(bits, pointer, 3)
+    typeid = bits2int(typeid)
+    @assert typeid != 4 "typeid ($typeid) == 4. This is a literal packet, not an operator."
+    
+    lengthid, pointer = read_n_bits(bits, pointer, 1)
+    lengthid = lengthid[1]
+    subvalues = []
+    if !lengthid
+        length, pointer = read_n_bits(bits, pointer, 15)
+        length = bits2int(length)
+        
+        bits_read = 0
+        while bits_read < length
+            value, new_pointer = parse_packet(bits, pointer)
+            bits_read += (new_pointer - pointer)
+            pointer = new_pointer
+            push!(subvalues, value)
+        end
+    else
+        n_subpackets, pointer = read_n_bits(bits, pointer, 11)
+        n_subpackets = bits2int(n_subpackets)
+
+        packets_read = 0
+        while packets_read < n_subpackets
+            value, pointer = parse_packet(bits, pointer)
+            packets_read += 1
+            push!(subvalues, value)
+        end
+    end
+    
+    return (version, typeid, subvalues), pointer
+end
+
+function version_sum(value::Tuple)
+    version, typeid, subvalue = value
+    if typeid == 4
+        return version
+    else
+        return version + version_sum(subvalue)
+    end
+end
+
+version_sum(values::Vector) = sum(version_sum(value) for value in values)
+
+
+
+sums = Vector{Int}([])
+for line in data
+    bits = hex2bits(line)
+    n_bits = length(bits)
+
+    value, pointer = parse_packet(bits, 1)
+    # @show value
+
+    push!(sums, version_sum(value))
+end
+
+@show sums
+
+answer = length(sums) == 1 ? first(sums) : sums
---
+++ day16_2.jl
@@ -0,0 +1,116 @@
+using DelimitedFiles
+
+data_file = "./data/test/day16_input.txt"
+# data_file = "./data/full/day16_input.txt"
+data = readdlm(data_file, '\n', String)[:]
+
+# Patch map, reduce and filter with Fix1 for piping and composition
+import Base.map, Base.parse
+map(f) = Base.Fix1(map, f)
+parse(t) = Base.Fix1(parse, t)
+
+hex2bits = BitVector ∘ map(parse(Bool)) ∘ collect ∘ join ∘ map(bitstring) ∘ hex2bytes
+bits2int = Base.Fix1(sum, ((i, x),) -> x << (i - 1)) ∘ enumerate ∘ reverse
+
+"""
+Read n bits from the bit stream, and push the pointer along the way.
+The pointer is the only hign which gets modified.
+"""
+function read_n_bits(bits::BitVector, pointer::Int, n::Int)
+    len = length(bits)
+    @assert (pointer+n-1) <= len "$pointer + $n larger than the length of the bits array! ($len)"
+    bits = bits[pointer:(pointer+n-1)]
+    pointer += n
+    return bits, pointer
+end
+
+function parse_packet(bits::BitVector, pointer::Int=1)::Tuple{Tuple{Int, Int, Any}, Int}
+    version, pointer = read_n_bits(bits, pointer, 3)
+    version = bits2int(version)
+    typeid, pointer = read_n_bits(bits, pointer, 3)
+    typeid = bits2int(typeid)
+
+    if typeid == 4
+        println("literal packet  | v$version id$typeid")
+        value, pointer = parse_literal_packet(bits, pointer)
+    elseif typeid <= 7
+        println("operator packet | v$version id$typeid")
+        value, pointer = parse_operator_packet(bits, pointer)
+    else
+        error("Unknown typeid ($typeid)")
+    end
+    return (version, typeid, value), pointer
+end
+
+"""
+Parse a literal packet out of the bit stream
+"""
+function parse_literal_packet(bits::BitVector, pointer::Int)::Tuple{Int, Int}
+    value = BitVector([])
+    indicator_bit = true
+    while indicator_bit
+        group, pointer = read_n_bits(bits, pointer, 5)
+        indicator_bit = group[1]
+        value = vcat(value, group[2:end])
+    end
+    value = bits2int(value)::Int
+    return value, pointer
+end
+
+"""
+Parse an operator packet out of the bit stream
+"""
+function parse_operator_packet(bits::BitVector, pointer::Int)::Tuple{Vector, Int}
+    lengthid, pointer = read_n_bits(bits, pointer, 1)
+    lengthid = lengthid[1]
+    
+    subvalues = []::Vector
+    length, pointer = read_n_bits(bits, pointer, lengthid ? 11 : 15)
+    length = bits2int(length)
+    
+    length_tally = 0
+    while length_tally < length
+        value, new_pointer = parse_packet(bits, pointer)
+        push!(subvalues, value)
+        length_tally += lengthid ? 1 : new_pointer - pointer
+        pointer = new_pointer
+    end
+    
+    return subvalues, pointer
+end
+
+function apply_operators(value::Tuple)::Int
+    _, typeid, subvalue = value
+    subvalue = apply_operators(subvalue)
+    if typeid == 0
+        return sum(subvalue)
+    elseif typeid == 1
+        return prod(subvalue)
+    elseif typeid == 2
+        return minimum(subvalue)
+    elseif typeid == 3
+        return maximum(subvalue)
+    elseif typeid == 4
+        return subvalue
+    elseif typeid == 5
+        @assert length(subvalue) == 2
+        return Int(subvalue[1] > subvalue[2])
+    elseif typeid == 6
+        @assert length(subvalue) == 2
+        return Int(subvalue[1] < subvalue[2])
+    elseif typeid == 7
+        @assert length(subvalue) == 2
+        return Int(subvalue[1] == subvalue[2])
+    else
+        error("unknown typeid ($typeid)")
+    end
+end
+
+apply_operators(values::Vector)::Vector{Int} = [apply_operators(value) for value in values]
+apply_operators(value::Int)::Int = value
+
+process = Int ∘ apply_operators ∘ first ∘ parse_packet ∘ hex2bits
+evals = map(process, data)
+@show evals
+
+answer = length(evals) == 1 ? first(evals) : evals
